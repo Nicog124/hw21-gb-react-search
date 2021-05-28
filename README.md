93ba42884e48a9ec2a4f2af3795c3b338ff57d3a
Nicholas Ganim <nicog99@gmail.com>
2021-05-27 21:51:39 -0400

    first real commit with code

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # hw21-gb-react-search
+
+# Description 
+In this activity, we create a new React-based Google Books Search app. This app construction used React components, helper functions, util functions, and utilized React lifecycle methods to query and display books based on user searches. We also use Node, Express and MongoDB so that users can save books to review or purchase later.
+
+# Screen Shots
+
+
+# Links
+Github - https://github.com/Nicog124/hw21-gb-react-search
+Heroku -
+
+# License 
+MIT
---
+++ client/build/precache-manifest.d86dfd698298160f16388a7704b09500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "240e24b4f4e5488e9f1c",
+    "url": "/static/css/main.82ae1d96.chunk.css"
+  },
+  {
+    "revision": "240e24b4f4e5488e9f1c",
+    "url": "/static/js/main.270b0952.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a76cbb8924a84bb40742",
+    "url": "/static/css/2.c95cc9b9.chunk.css"
+  },
+  {
+    "revision": "a76cbb8924a84bb40742",
+    "url": "/static/js/2.387278db.chunk.js"
+  },
+  {
+    "revision": "fc7c78cd547df8d4ccaad13645b9bae2",
+    "url": "/index.html"
+  }
+];
